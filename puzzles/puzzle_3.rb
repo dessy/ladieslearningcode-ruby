604e2b0fa210e8386dcab39465a134872f6a9154
Dessy Daskalov <dessy.daskalov@gmail.com>
2011-10-12 00:58:16 -0400

    Arrays and final blackjack game

---
+++ puzzles/puzzle_3.rb
@@ -0,0 +1,19 @@
+names = ["Lisa", "Bart", "Maggie", "Lisa", "Marge", "Homer"]
+
+puts names[2]
+puts names.join(', ')
+puts names.delete_at(1)
+puts names
+puts names[2]
+
+names << "Milhouse"
+
+puts names
+puts names.uniq
+puts names.join(' - ')
+
+names.each do |name|
+  if name.length != 5
+    puts name
+  end
+end
