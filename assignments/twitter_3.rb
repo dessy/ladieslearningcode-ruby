6b609277c466aa2276223ed6a856a2386179d3a4
Dessy Daskalov <dessy.daskalov@gmail.com>
2011-10-15 19:21:50 -0400

    Conditional logic

--- assignments/twitter_3.rb
+++ assignments/twitter_3.rb
@@ -1,9 +1,12 @@
 puts "Gimme your tweet!"
 
-tweet = gets
+tweet = gets.chomp
 
+puts "This is your tweet:"
 puts tweet
 
+puts "The length of your tweet is:"
 puts tweet.length
 
+puts "The number of characters you have until you hit 140 is:"
 puts 140 - tweet.length
