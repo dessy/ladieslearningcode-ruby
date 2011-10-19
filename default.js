219d0e2b6ad985eea24b27e25fca759bc698340c
Dessy Daskalov <dessy.daskalov@gmail.com>
2011-10-18 20:41:19 -0400

    Grab fonts from google

--- default.js
+++ default.js
@@ -546,8 +546,7 @@ function addFontStyle() {
   var el = document.createElement('link');
   el.rel = 'stylesheet';
   el.type = 'text/css';
-  el.href = 'http://fonts.googleapis.com/css?family=' +
-            'Open+Sans:regular,semibold,italic,italicsemibold|Droid+Sans+Mono';
+  el.href = 'fonts.css';
 
   document.body.appendChild(el);
 };
