2612c71e5ca6ba1b81289a47b60580f49f5db390
Dessy Daskalov <dessy.daskalov@gmail.com>
2014-01-31 16:55:20 -0500

    Update js file

--- default.js
+++ default.js
@@ -10,7 +10,7 @@
   URL: http://code.google.com/p/html5slides/
 */
 
-var PERMANENT_URL_PREFIX = ''; // 'http://html5slides.googlecode.com/svn/trunk/';
+var PERMANENT_URL_PREFIX = '';
 
 var SLIDE_CLASSES = ['far-past', 'past', 'current', 'next', 'far-next'];
 
@@ -19,7 +19,7 @@ var PM_TOUCH_SENSITIVITY = 15;
 var curSlide;
 
 /* ---------------------------------------------------------------------- */
-/* classList polyfill by Eli Grey 
+/* classList polyfill by Eli Grey
  * (http://purl.eligrey.com/github/classList.js/blob/master/classList.js) */
 
 if (typeof document !== "undefined" && !("classList" in document.createElement("a"))) {
@@ -143,7 +143,7 @@ if (objCtr.defineProperty) {
 /* Slide movement */
 
 function getSlideEl(no) {
-  if ((no < 0) || (no >= slideEls.length)) { 
+  if ((no < 0) || (no >= slideEls.length)) {
     return null;
   } else {
     return slideEls[no];
@@ -152,15 +152,15 @@ function getSlideEl(no) {
 
 function updateSlideClass(slideNo, className) {
   var el = getSlideEl(slideNo);
-  
+
   if (!el) {
     return;
   }
-  
+
   if (className) {
     el.classList.add(className);
   }
-    
+
   for (var i in SLIDE_CLASSES) {
     if (className != SLIDE_CLASSES[i]) {
       el.classList.remove(SLIDE_CLASSES[i]);
@@ -177,14 +177,14 @@ function updateSlides() {
       case curSlide - 1:
         updateSlideClass(i, 'past');
         break;
-      case curSlide: 
+      case curSlide:
         updateSlideClass(i, 'current');
         break;
       case curSlide + 1:
-        updateSlideClass(i, 'next');      
+        updateSlideClass(i, 'next');
         break;
       case curSlide + 2:
-        updateSlideClass(i, 'far-next');      
+        updateSlideClass(i, 'far-next');
         break;
       default:
         updateSlideClass(i);
@@ -202,10 +202,10 @@ function updateSlides() {
 
   enableSlideFrames(curSlide - 1);
   enableSlideFrames(curSlide + 2);
-  
+
   if (isChromeVoxActive()) {
     speakAndSyncToNode(slideEls[curSlide]);
-  }  
+  }
 
   updateHash();
 };
@@ -217,7 +217,7 @@ function buildNextItem() {
     return false;
   }
 
-  toBuild[0].classList.remove('to-build', '');
+  toBuild[0].classList.remove('to-build');
 
   if (isChromeVoxActive()) {
     speakAndSyncToNode(toBuild[0]);
@@ -280,7 +280,7 @@ function triggerLeaveEvent(no) {
   var evt = document.createEvent('Event');
   evt.initEvent('slideleave', true, true);
   evt.slideNumber = no + 1; // Make it readable
-  
+
   el.dispatchEvent(evt);
 };
 
@@ -319,13 +319,13 @@ function handleTouchEnd(event) {
       nextSlide();
     }
   }
-  
+
   cancelTouch();
 };
 
 function cancelTouch() {
   document.body.removeEventListener('touchmove', handleTouchMove, true);
-  document.body.removeEventListener('touchend', handleTouchEnd, true);  
+  document.body.removeEventListener('touchend', handleTouchEnd, true);
 };
 
 /* Preloading frames */
@@ -372,29 +372,29 @@ function setupFrames() {
     frame._src = frame.src;
     disableFrame(frame);
   }
-  
+
   enableSlideFrames(curSlide);
   enableSlideFrames(curSlide + 1);
-  enableSlideFrames(curSlide + 2);  
+  enableSlideFrames(curSlide + 2);
 };
 
 function setupInteraction() {
   /* Clicking and tapping */
-  
+
   var el = document.createElement('div');
   el.className = 'slide-area';
-  el.id = 'prev-slide-area';  
+  el.id = 'prev-slide-area';
   el.addEventListener('click', prevSlide, false);
   document.querySelector('section.slides').appendChild(el);
 
   var el = document.createElement('div');
   el.className = 'slide-area';
-  el.id = 'next-slide-area';  
+  el.id = 'next-slide-area';
   el.addEventListener('click', nextSlide, false);
-  document.querySelector('section.slides').appendChild(el);  
-  
+  document.querySelector('section.slides').appendChild(el);
+
   /* Swiping */
-  
+
   document.body.addEventListener('touchstart', handleTouchStart, false);
 }
 
@@ -412,9 +412,9 @@ function speakAndSyncToNode(node) {
   if (!isChromeVoxActive()) {
     return;
   }
-  
+
   cvox.ChromeVox.navigationManager.switchToStrategy(
-      cvox.ChromeVoxNavigationManager.STRATEGIES.LINEARDOM, 0, true);  
+      cvox.ChromeVoxNavigationManager.STRATEGIES.LINEARDOM, 0, true);
   cvox.ChromeVox.navigationManager.syncToNode(node);
   cvox.ChromeVoxUserCommands.finishNavCommand('');
   var target = node;
@@ -428,7 +428,7 @@ function speakNextItem() {
   if (!isChromeVoxActive()) {
     return;
   }
-  
+
   cvox.ChromeVox.navigationManager.switchToStrategy(
       cvox.ChromeVoxNavigationManager.STRATEGIES.LINEARDOM, 0, true);
   cvox.ChromeVox.navigationManager.next(true);
@@ -448,7 +448,7 @@ function speakPrevItem() {
   if (!isChromeVoxActive()) {
     return;
   }
-  
+
   cvox.ChromeVox.navigationManager.switchToStrategy(
       cvox.ChromeVoxNavigationManager.STRATEGIES.LINEARDOM, 0, true);
   cvox.ChromeVox.navigationManager.previous(true);
@@ -520,7 +520,7 @@ function handleBodyKeyDown(event) {
 };
 
 function addEventListeners() {
-  document.addEventListener('keydown', handleBodyKeyDown, false);  
+  document.addEventListener('keydown', handleBodyKeyDown, false);
 };
 
 /* Initialization */
@@ -532,7 +532,7 @@ function addPrettify() {
       el.classList.add('prettyprint');
     }
   }
-  
+
   var el = document.createElement('script');
   el.type = 'text/javascript';
   el.src = PERMANENT_URL_PREFIX + 'prettify.js';
@@ -546,7 +546,8 @@ function addFontStyle() {
   var el = document.createElement('link');
   el.rel = 'stylesheet';
   el.type = 'text/css';
-  el.href = 'fonts.css';
+  el.href = 'http://fonts.googleapis.com/css?family=' +
+            'Open+Sans:regular,semibold,italic,italicsemibold|Droid+Sans+Mono';
 
   document.body.appendChild(el);
 };
@@ -557,12 +558,12 @@ function addGeneralStyle() {
   el.type = 'text/css';
   el.href = PERMANENT_URL_PREFIX + 'styles.css';
   document.body.appendChild(el);
-  
+
   var el = document.createElement('meta');
   el.name = 'viewport';
   el.content = 'width=1100,height=750';
   document.querySelector('head').appendChild(el);
-  
+
   var el = document.createElement('meta');
   el.name = 'apple-mobile-web-app-capable';
   el.content = 'yes';
@@ -602,7 +603,7 @@ function initialize() {
   getCurSlideFromHash();
 
   if (window['_DEBUG']) {
-    // PERMANENT_URL_PREFIX = '../';
+    PERMANENT_URL_PREFIX = '../';
   }
 
   if (window['_DCL']) {
